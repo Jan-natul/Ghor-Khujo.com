080899c4155cb86b1810a97d14b45e3d6ecc37f3
Jan-natul <jannatulferdous02020@gmail.com>
2025-09-14 00:51:15 +0600

    Completed

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,83 @@
+ import bcrypt from "bcryptjs";
+ import jwt from "jsonwebtoken";
+import prisma from "../lib/prisma.js";
+
+export async function register(req,res){
+    const { username, email, password} = req.body;
+
+    try{
+
+    //Hash password
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    console.log(hashedPassword);
+    //Create a new user and save to db
+    const newUser= await prisma.user.create({
+        data:{
+           username,
+           email,
+           password:hashedPassword, 
+        }
+    });
+
+    console.log(newUser);
+    res.status (201).json({ message: "User created successfully"});
+  }catch(err){
+    console.log(err);
+    res.status(500).json({ message:"Failed to create user!"});
+
+  } 
+};
+
+export async function login(req,res){
+    console.log("Login route hit");
+    console.log("Body:", req.body);
+
+    const { username, password} = req.body;
+
+    try{
+
+        //Check if the user exists
+
+        const user = await prisma.user.findUnique({
+            where: {username},
+        });
+
+        if (!user) return res.status(401).json({message: "Invalid Credentials!"});
+
+        //check if the password is correct
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid) return res.status(401).json({message: "Invalid Credentials!"});
+
+       //generate cookie token and send to the user
+       
+       //res.setHeader("Set-Cookie", "test=" + "myValue").json("success");
+       const age = 1000*60*60*24*7;
+
+       const token = jwt.sign({
+        id:user.id,
+        isAdmin: false,
+       }, process.env.JWT_SECRET_KEY,{expiresIn: age});
+      
+       const {password: userPassword, ...userInfo } = user;
+       
+       res.
+       cookie("token",token,{
+       httpOnly:true,
+       maxAge: age,
+       })
+       .status(200)
+       .json(userInfo);
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message:"Failed to login!"});
+    }
+};
+
+export const logout = (req,res)=>{
+    res.clearCookie("token").status(200).json({message:"Logout Successfull!"});
+};
---
+++ api/routes/user.route.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+  savePost,
+  profilePosts,
+  checkSaved,
+  getNotificationNumber
+} from "../controllers/user.controller.js";
+import {verifyToken} from "../middleware/verifyToken.js";
+
+const router = express.Router();
+
+router.get("/", getUsers);
+
+router.put("/:id", verifyToken, updateUser);
+router.delete("/:id", verifyToken, deleteUser);
+router.post("/save", verifyToken, savePost);
+router.get("/profilePosts", verifyToken, profilePosts);
+router.get("/notification", verifyToken, getNotificationNumber);
+router.get("/isSaved/:postId", verifyToken, checkSaved);
+
+export default router;
